1c66ade900720de818e83cca8d63b9ce7591ea70
kamal126 <kamalchandra265@gmail.com>
2025-10-14 11:49:54 +0530

    fix: simplify test cases by removing unnecessary imports

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,14 +1,11 @@
-import pytest
-import src.math_operations as math_ops
+from src.math_operations import add,sub
 
 def test_add():
-    assert math_ops.add(2, 3) == 5
-    assert math_ops.add(-1, 1) == 0
-    assert math_ops.add(0, 0) == 0
-    assert math_ops.add(-1, -1) == -2
-
+    assert add(2,3)==5
+    assert add(-1,1)==0
+    
 def test_sub():
-    assert math_ops.sub(5, 3) == 2
-    assert math_ops.sub(0, 0) == 0
-    assert math_ops.sub(-1, -1) == 0
-    assert math_ops.sub(-1, 1) == -2
+    assert sub(5,3)==2
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
